01f9f27110d7a0c5d183a8c89f3b9d5f48e53dc0
Administrator <rr@>
2024-11-01 17:02:22 +0800

    light 样式修改

--- src/view/light/index.component.ts
+++ src/view/light/index.component.ts
@@ -2,10 +2,10 @@
 // Copyright @ 2018-present xiejiahe. All rights reserved.
 // See https://github.com/xjh22222228/nav
 
-import { Component } from '@angular/core'
-import { randomBgImg } from 'src/utils'
-import { CommonService } from 'src/services/common'
-import { JumpService } from 'src/services/jump'
+import {Component} from '@angular/core'
+import {randomBgImg} from 'src/utils'
+import {CommonService} from 'src/services/common'
+import {JumpService} from 'src/services/jump'
 
 @Component({
   selector: 'app-light',
@@ -16,10 +16,16 @@ export default class LightComponent {
   constructor(
     public commonService: CommonService,
     public jumpService: JumpService
-  ) {}
+  ) {
+  }
 
   ngOnInit() {
-    randomBgImg()
+    // randomBgImg()
+    document.body.style.backgroundImage = 'url("assets/img/background.jpg")';
+    document.body.style.backgroundAttachment = 'fixed';/*背景图片不会随着页面的滚动而滚动。*/
+    document.body.style.backgroundRepeat = 'no-repeat';/*设置背景不重复*/
+    document.body.style.backgroundSize = 'cover';/*	此时会保持图像的纵横比并将图像缩放成将完全覆盖背景定位区域的最小大小。*/
+
   }
 
   ngOnDestroy() {
